79bb45bfda8f538c8ed5f34a570cd53e515f4e1b
Niklas Kettunen <niklas.kettunen@gmail.com>
2021-05-07 22:35:22 +0300

    updated calibration string output

--- version_0.1_wifi/version_0.1_wifi.ino
+++ version_0.1_wifi/version_0.1_wifi.ino
@@ -249,7 +249,9 @@ void loop() {
     Serial.printf("Pressure = %.2f hPa \n", pressure);
     Serial.printf("Index of Air Quality = %d \n", iaq);
     Serial.printf("Calibration status = %d \n", calibration_status);
-    Serial.printf("Calibration text = " + calibration_text + "\n");
+    Serial.print("Calibration text = ");
+    Serial.print(calibration_text);
+    Serial.println();
     Serial.println();
 
     // Send Events to the Web Server with the Sensor Readings
